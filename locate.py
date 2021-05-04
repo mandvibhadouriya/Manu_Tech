cf36930909cda0b3bad547876cd194b0dd2c0a2a
Mandvi Bhadouriya <79691249+manuthakur0909@users.noreply.github.com>
2021-05-04 21:57:05 +0530

    Add files via upload

---
+++ locate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import phonenumbers
+
+
+# In[2]:
+
+
+from phonenumbers import geocoder
+
+
+# In[8]:
+
+
+num1 = phonenumbers.parse("+123456789")
+print(geocoder.description_for_number(num1,'en'))
+
